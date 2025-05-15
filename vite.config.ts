dd1e55ea7a71265be6890474cd9d63fb4f8a51b9
jjappleg <149392794+jjappleg@users.noreply.github.com>
2025-05-14 23:12:13 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/RequirementsSyntaxChecker/' // Correct Base for GITHUB Site
+  base: '/RequirementsSyntaxChecker/',  // Correct base URL for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
