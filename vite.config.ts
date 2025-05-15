68350adc337719af9777344af6c03911c6dddf18
jjappleg <149392794+jjappleg@users.noreply.github.com>
2025-05-14 22:47:32 -0400

    Update vite.config.ts
    added base to ensure proper link

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/RequirementsSyntaxChecker/' // Correct Base for GITHUB Site
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
